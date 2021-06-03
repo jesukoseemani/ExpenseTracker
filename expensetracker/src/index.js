57614f799bce94e0c163c9a73f5383a5b6f09817
jesukoseemani <olowosusiayo@gmail.com>
2021-06-03 12:25:44 +0100

    Update index.js

--- expensetracker/src/index.js
+++ expensetracker/src/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import { SpeechProvider } from '@speechly/react-client';
 
 import { Provider } from './context/context';
+
+
 import App from './App';
 import './index.css';
 
 ReactDOM.render(
   <SpeechProvider appId="8ba8d72a-b6d9-45e9-8635-89b10f33123c" language="en-US">
+    
+    
     <Provider>
       <App />
     </Provider>
+  
+  
   </SpeechProvider>,
   document.getElementById('root'),
 );
